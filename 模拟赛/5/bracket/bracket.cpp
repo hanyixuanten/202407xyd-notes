7f9b5102c42ab0a8102a69e277608ab2f891438e
hanyixuanten <hanyixuanten@outlook.com>
2024-08-05 11:12:01 +0800

    edit 8/2 模拟赛

--- 模拟赛/5/bracket/bracket.cpp
+++ 模拟赛/5/bracket/bracket.cpp
@@ -2,29 +2,56 @@
 using namespace std;
 int n, L;
 char s[1000005];
-int fans = 0;
-bool dept(int l, int r)
+int dept(int l, int r)
 {
+    if ((r - l + 1) % 2 != 0)
+        return -1;
     if (r == l + 1)
+        return (s[l] == '(' && s[r] == ')') ? 1 : -1;
+    if (s[l] == '(' && s[r] == ')')
     {
-        fans = max(fans, 1);
-        return (s[l] == '(' && s[r] == ')') ? 1 : 0;
+        int tmp = dept(l + 1, r - 1);
+        if (tmp != -1)
+            return tmp + 1;
     }
     int dep = 0;
     int ll = l;
     bool ans = 1;
+    int maxdep = 0;
     for (int i = l; i <= r; ++i)
     {
         if (dep == 0 && i != l && i != r)
-            ans = ans && dept(ll, i), ll = i;
+        {
+            int tmp = dept(ll, i);
+            ans = ans && (tmp != -1), ll = i;
+        }
         if (s[i] == '(')
-            ++dep, fans = max(fans, dep);
+            ++dep, maxdep = max(maxdep, dep);
         else
             --dep;
         if (dep < 0 || !ans)
-            return 0;
+            return -1;
     }
-    return dep == 0 && ans;
+    return (ans && dep == 0) ? maxdep : -1;
+}
+int ans = INT_MAX;
+void dfs(int dep, int nowid) // 30pts - bigeps
+{
+    if (nowid > n)
+        return;
+    if (dep > ans)
+        return;
+    int tmp = dept(1, n);
+    if (tmp <= L && tmp != -1)
+    {
+        ans = min(ans, dep);
+        return;
+    }
+    dfs(dep, nowid + 1);
+    s[nowid] = (s[nowid] == '(' ? ')' : '(');
+    dfs(dep + 1, nowid + 1);
+    s[nowid] = (s[nowid] == '(' ? ')' : '(');
+    return;
 }
 int main()
 {
@@ -32,6 +59,7 @@ int main()
     freopen("bracket.out", "w", stdout);
     scanf("%d%d\n", &n, &L);
     cin >> s + 1;
-    printf("%d %d\n", dept(1, n), fans);
+    dfs(0, 1);
+    printf("%d\n", ans);
     return 0;
 }
